f19b920e9f5daf04c8df18f8e992eac489ce455f
CastellanR <castellanra@gmail.com>
2020-05-21 12:43:08 -0400

    Service structure added

---
+++ backend/user/src/services/user/user.go
@@ -0,0 +1,16 @@
+package user
+
+// CreateUser save an user to the database
+func CreateUser() {
+
+}
+
+// GetUserByID returns the user's info searched by ID
+func GetUserByID() {
+
+}
+
+// GetListUser returns the user list of the database	
+func GetListUser() {
+
+}
