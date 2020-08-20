ef4ef19e0379a993b362947ce38a34df6ea741f6
CastellanR <castellanra@gmail.com>
2020-08-20 21:49:51 +0100

    React hook form and styled components added

--- frontend/src/app/signin/Signin.js
+++ frontend/src/app/signin/Signin.js
@@ -1,74 +1,67 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { IconContext } from "react-icons";
 import { FaFacebook } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
-import "./Signin.scss";
-class Signin extends Component {
-  render() {
-    return (
-      <div class="wrapper">
-        <div className="home">
-          <a href="/">Home</a>
-        </div>
-        <p>
-          Sign in or <a href="#">Create your account</a>
-        </p>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Email:
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              className="formInput"
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="text"
-              name="password"
-              placeholder="Enter your password"
-              className="formInput"
-            />
-          </label>
-          <label>
-            Keep session active:
-            <input
-              type="checkbox"
-              name="keepSessionActive"
-              className="formInput"
-            />
-          </label>
-          <div className="submit">
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-        <p class="separator">
-          <span>Or</span>
-        </p>
-        <div className="mediaButtons">
-          <div class="google">
-            <IconContext.Provider value={{ className: "googleIcon" }}>
-              <FcGoogle />
-            </IconContext.Provider>
-            <a href="#" class="link">
-              Continue with Google
-            </a>
-          </div>
-          <div class="facebook">
-            <IconContext.Provider value={{ className: "faceIcon" }}>
-              <FaFacebook />
-            </IconContext.Provider>
-            <a href="#" class="link">
-              Continue with Facebook
-            </a>
-          </div>
-        </div>
+import { Button, FormInput } from "./style";
+
+const Signin = () => {
+  const { register, handleSubmit, watch, errors } = useForm();
+  
+  return (
+    <div class="wrapper">
+      <div className="home">
+        <a href="/">Home</a>
       </div>
-    );
-  }
-}
+      <h2>
+        Sign in or <a href="#">Create your account</a>
+      </h2>
+      <form /*onSubmit={this.handleSubmit}*/>
+        <label>
+          Email:
+          <FormInput type="text" name="email" placeholder="Enter your email" />
+        </label>
+        <label>
+          Password:
+          <FormInput
+            type="text"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </label>
+        <label>
+          Keep session active:
+          <FormInput type="checkbox" name="keepSessionActive" />
+        </label>
+        <div className="submit">
+          <Button type="submit" value="Submit" />
+        </div>
+      </form>
+      <p class="separator">
+        <span>Or</span>
+      </p>
+      <div className="mediaButtons">
+        <div class="google">
+          <IconContext.Provider value={{ className: "googleIcon" }}>
+            <FcGoogle />
+          </IconContext.Provider>
+          <a href="#" class="link">
+            Continue with Google
+          </a>
+        </div>
+        <div class="facebook">
+          <IconContext.Provider value={{ className: "faceIcon" }}>
+            <FaFacebook />
+          </IconContext.Provider>
+          <a href="#" class="link">
+            Continue with Facebook
+          </a>
+        </div>
+        <Button>Probando la mecha</Button>
+      </div>
+    </div>
+  );
+};
 
 export default Signin;
---
+++ frontend/src/app/signin/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  width: 4em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export const FormInput = styled.input`
+  background-color: white;
+  margin: 0.5em 0em 0.5em 1em;
+  border-width: 0em 0em 0.1em 0em;
+  border-color: black;
+  border-style: solid;
+  width: 10em;
+`;
